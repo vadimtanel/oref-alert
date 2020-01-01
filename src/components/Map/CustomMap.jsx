4169b7eb7ad8782a1001db5983c1caa4c184f13a
Vadim Tanel <vadimtanel@gmail.com>
2020-01-01 14:26:30 +0200

    fix map + add node-sass

--- src/components/Map/CustomMap.jsx
+++ src/components/Map/CustomMap.jsx
@@ -1,41 +1,39 @@
 import React, { useState, useEffect } from "react";
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-import L from 'leaflet'
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 
-const classes = require('./style.css');
-
+const classes = require("./style.scss");
 
 export default function CustomMap() {
   const [lat, setLat] = useState(51.505);
   const [lng, setLng] = useState(-0.09);
   const [zoom, setLZoom] = useState(13);
-  
+
   const pointerIcon = new L.Icon({
-    iconUrl: './../../asserts/mapPointer.png',
-    iconRetinaUrl: './../../asserts/mapPointer.png',
+    iconUrl: "./../../asserts/mapPointer.png",
+    iconRetinaUrl: "./../../asserts/mapPointer.png",
     iconAnchor: [5, 55],
     popupAnchor: [10, -44],
     iconSize: [25, 55],
-    shadowUrl: './../../assets/marker-shadow.png',
+    shadowUrl: "./../../assets/marker-shadow.png",
     shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
-  })
-
-  useEffect(() => {
+    shadowAnchor: [20, 92]
   });
 
-  const position = [lat, lng]
+  useEffect(() => {});
+
+  const position = [lat, lng];
   return (
-      <Map center={position} zoom={zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position} icon={pointerIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
+    <Map center={position} zoom={zoom}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={pointerIcon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
   );
 }
